996838eb1ecff8db44ed62fdc06f3a7759b45498
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-24 18:45:55 +0900

    navbar keeps wrapping

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,93 +23,93 @@ const Navbar = () => {
   };
 
   return (
-    <div
-      className={
-        isMobile ? styles.navBarMobileContainer : styles.navBarPageContainer
-      }
-    >
-      <div
-        className={isMobile ? styles.navBarMobileContent : styles.navBarContent}
-      >
-        <MediaQuery minWidth={901}>
-          <div className={styles.logo}>
-            <Link href="/" passHref={true}>
-              <h3>NegiNeko_Tokyo</h3>
-            </Link>
+    <>
+      <MediaQuery minWidth={901}>
+        <div className={styles.navBarPageContainer}>
+          <div className={styles.navBarContent}>
+            <div className={styles.logo}>
+              <Link href="/" passHref={true}>
+                <h3>NegiNeko_Tokyo</h3>
+              </Link>
+            </div>
+            <div className={styles.pageSelector}>
+              <h4>
+                <Link href="/"> Home </Link>
+              </h4>
+              <h4>
+                <Link href="/about"> About </Link>
+              </h4>
+              <h4>
+                <Link href="/travel"> Travel-Log </Link>
+              </h4>
+              <h4>
+                <Link href="/multiView"> Multi-view </Link>
+              </h4>
+            </div>
+            <div className={styles.signInButton}>
+              <button>Sign In</button>
+            </div>
           </div>
-          <div className={styles.pageSelector}>
-            <h4>
-              <Link href="/"> Home </Link>
-            </h4>
-            <h4>
-              <Link href="/about"> About </Link>
-            </h4>
-            <h4>
-              <Link href="/travel"> Travel-Log </Link>
-            </h4>
-            <h4>
-              <Link href="/multiView"> Multi-view </Link>
-            </h4>
-          </div>
-          <div className={styles.signInButton}>
-            <button>Sign In</button>
-          </div>
-        </MediaQuery>
-        <MediaQuery maxWidth={900}>
-          <div className={styles.logo}>
-            <Link href="/" passHref={true}>
-              <h3 className={styles.mobileHeader}>NegiNeko_Tokyo</h3>
-            </Link>
-          </div>
-          <div
-            className={styles.barContainer}
-            onClick={() => {
-              navBarHandler("open");
-            }}
-          >
-            <div className={styles.bar} />
-            <div className={styles.bar} />
-            <div className={styles.bar} />
-          </div>
-          <div
-            className={`${styles.blurOverlay} openNavBar`}
-            onClick={() => {
-              navBarHandler("close");
-            }}
-          />
-          <div className={`${styles.mobileNavBarSelector} openNavBar`}>
-            <p
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={900}>
+        <div className={styles.navBarMobileContainer}>
+          <div className={styles.navBarMobileContent}>
+            <div className={styles.logo}>
+              <Link href="/" passHref={true}>
+                <h3 className={styles.mobileHeader}>NegiNeko_Tokyo</h3>
+              </Link>
+            </div>
+            <div
+              className={styles.barContainer}
+              onClick={() => {
+                navBarHandler("open");
+              }}
+            >
+              <div className={styles.bar} />
+              <div className={styles.bar} />
+              <div className={styles.bar} />
+            </div>
+            <div
+              className={`${styles.blurOverlay} openNavBar`}
               onClick={() => {
                 navBarHandler("close");
               }}
-            >
-              <Link href="/"> Home </Link>
-            </p>
-            <p
-              onClick={() => {
-                navBarHandler("close");
-              }}
-            >
-              <Link href="/about"> About </Link>
-            </p>
-            <p
-              onClick={() => {
-                navBarHandler("close");
-              }}
-            >
-              <Link href="/travel"> Travel-log </Link>
-            </p>
-            <p
-              onClick={() => {
-                navBarHandler("close");
-              }}
-            >
-              <Link href="/multiView"> Multi-view </Link>
-            </p>
+            />
+            <div className={`${styles.mobileNavBarSelector} openNavBar`}>
+              <p
+                onClick={() => {
+                  navBarHandler("close");
+                }}
+              >
+                <Link href="/"> Home </Link>
+              </p>
+              <p
+                onClick={() => {
+                  navBarHandler("close");
+                }}
+              >
+                <Link href="/about"> About </Link>
+              </p>
+              <p
+                onClick={() => {
+                  navBarHandler("close");
+                }}
+              >
+                <Link href="/travel"> Travel-log </Link>
+              </p>
+              <p
+                onClick={() => {
+                  navBarHandler("close");
+                }}
+              >
+                <Link href="/multiView"> Multi-view </Link>
+              </p>
+            </div>
           </div>
-        </MediaQuery>
-      </div>
-    </div>
+        </div>
+      </MediaQuery>
+    </>
   );
 };
 
