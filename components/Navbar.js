0dafbfd3fa0b6abe23294da831a4326880b47fb4
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-24 17:35:43 +0900

    Added navbar animation for mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,112 @@
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/navbar.module.css";
+import MediaQuery, { useMediaQuery } from "react-responsive";
+import { useState } from "react";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
+  // const [openNavBar, setOpenNavBar] = useState(false);
+
+  const navBarHandler = (value) => {
+    value === "open"
+      ? gsap.fromTo(
+          ".openNavBar",
+          { display: "none", opacity: 0, y: -100 },
+          { display: "block", opacity: 1, y: 0 }
+        )
+      : gsap.fromTo(
+          ".openNavBar",
+          { display: "block", opacity: 1, y: 0 },
+          { display: "none", opacity: 0, y: -100 }
+        );
+  };
+
   return (
-    <div className={styles.navBarPageContainer}>
-      <div className={styles.navBarContainer}>
+    <div
+      className={
+        isMobile ? styles.navBarMobileContainer : styles.navBarPageContainer
+      }
+    >
+      <div
+        className={isMobile ? styles.navBarMobileContent : styles.navBarContent}
+      >
         <div className={styles.logo}>
           <Link href="/" passHref={true}>
-            <h3>NegiNeko_Tokyo</h3>
+            <h3 className={isMobile ? styles.mobileHeader : ""}>
+              NegiNeko_Tokyo
+            </h3>
           </Link>
         </div>
-        <div className={styles.pageSelector}>
-          <h4>
-            <Link href="/"> Home </Link>
-          </h4>
-          <h4>
-            <Link href="/about"> About </Link>
-          </h4>
-          <h4>
-            <Link href="/travel"> Travel-Log </Link>
-          </h4>
-          {/* <h4>
-            <Link href="/merch"> Merch </Link>
-          </h4> */}
-          <h4>
-            <Link href="/multiView"> Multi-view </Link>
-          </h4>
-        </div>
-        <div className={styles.signInButton}>
-          <button>Sign In</button>
-        </div>
+        <MediaQuery minWidth={901}>
+          <div className={styles.pageSelector}>
+            <h4>
+              <Link href="/"> Home </Link>
+            </h4>
+            <h4>
+              <Link href="/about"> About </Link>
+            </h4>
+            <h4>
+              <Link href="/travel"> Travel-Log </Link>
+            </h4>
+            <h4>
+              <Link href="/multiView"> Multi-view </Link>
+            </h4>
+          </div>
+          <div className={styles.signInButton}>
+            <button>Sign In</button>
+          </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={900}>
+          <div
+            className={styles.barContainer}
+            onClick={() => {
+              navBarHandler("open");
+            }}
+          >
+            <div className={styles.bar} />
+            <div className={styles.bar} />
+            <div className={styles.bar} />
+          </div>
+          <div
+            className={`${styles.blurOverlay} openNavBar`}
+            onClick={() => {
+              navBarHandler("close");
+            }}
+          />
+          <div className={`${styles.mobileNavBarSelector} openNavBar`}>
+            <p
+              onClick={() => {
+                navBarHandler("close");
+              }}
+            >
+              <Link href="/"> Home </Link>
+            </p>
+            <p
+              onClick={() => {
+                navBarHandler("close");
+              }}
+            >
+              <Link href="/about"> About </Link>
+            </p>
+            <p
+              onClick={() => {
+                navBarHandler("close");
+              }}
+            >
+              <Link href="/travel"> Travel-log </Link>
+            </p>
+            <p
+              onClick={() => {
+                navBarHandler("close");
+              }}
+            >
+              <Link href="/multiView"> Multi-view </Link>
+            </p>
+          </div>
+        </MediaQuery>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "../styles/homepage.module.css";
 
 const Home = () => {
-  return <div>Home Page is under construction</div>;
+  return (
+    <div className={styles.homePageContainer}>
+      Home Page is under construction
+    </div>
+  );
 };
 
 export default Home;
