547b2360f92a81297bc329a305309b1a79558fb2
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-25 13:41:47 +0900

    Added netlify plugin

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const nextConfig = {
       {
         // matching all API routes
         source: "/api/:path*",
+        future: { webpack5: true },
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
