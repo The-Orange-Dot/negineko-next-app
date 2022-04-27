8bfcf391bbdd41c3839e77e88e86a4a2d5a9c28f
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-27 21:02:41 +0900

    Loading text for about page

--- components/locations/searchBar.js
+++ components/locations/searchBar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/travel.module.css";
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <div className={styles.searchBarContainer}>
       <input type="text" name="search" placeholder="e.g. Gundam Base" />
