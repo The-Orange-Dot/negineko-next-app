3336ee23e71f42de5726feca0dcd8a1ac559ca19
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-27 22:01:32 +0900

    Added search function for locations

--- components/locations/searchBar.js
+++ components/locations/searchBar.js
@@ -1,13 +1,49 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import styles from "../../styles/travel.module.css";
+import Fuse from "fuse.js";
 
-const SearchBar = () => {
+const SearchBar = ({ data, setLocations }) => {
   const [searchInput, setSearchInput] = useState("");
+  const options = {
+    keys: ["name", "caption"],
+    threshold: 0.5,
+  };
+  const fuse = new Fuse(data, options);
+
+  useEffect(() => {
+    setLocations(data);
+    console.log(data);
+  }, [setLocations, data]);
+
+  const searchHandler = (e, text) => {
+    e.preventDefault();
+    if (text === "") {
+      setLocations(data);
+    } else {
+      setLocations(fuse.search(text));
+    }
+    console.log(text);
+  };
 
   return (
     <div className={styles.searchBarContainer}>
-      <input type="text" name="search" placeholder="e.g. Gundam Base" />
-      <button>Submit</button>
+      <form
+        action=""
+        onSubmit={(e) => {
+          searchHandler(e, searchInput);
+        }}
+      >
+        <input
+          type="text"
+          name="search"
+          placeholder="e.g. Gundam Base"
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
--- pages/travel/index.js
+++ pages/travel/index.js
@@ -24,71 +24,106 @@ const Travel = ({ data }) => {
   const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
 
   useEffect(() => {
-    setLocations(data);
     isMobile ? setMobile(true) : setMobile(false);
     setPageLoaded(true);
-  }, [isMobile, data]);
+  }, [isMobile, data, locations]);
 
-  const travelLocations = locations?.map((location) => (
-    <div
-      key={location.id}
-      className={
-        mobile
-          ? styles.mobileLocationCardContainer
-          : styles.locationCardContainer
-      }
-    >
+  const travelLocations =
+    locations.length === 0 ? (
       <div>
-        <Link href={`/travel/${location.id}`} passHref={true}>
-          <h3>{location.name}</h3>
-        </Link>
-        <p>{location.caption}</p>
-        <p>{location.address}</p>
-        {pageLoaded ? (
+        <h1>No matches found</h1>
+      </div>
+    ) : (
+      locations.map((location) => (
+        <div
+          key={location.item ? location.item.id : location.id}
+          className={
+            mobile
+              ? styles.mobileLocationCardContainer
+              : styles.locationCardContainer
+          }
+        >
           <div>
-            {/*Website Check*/}
-            {location.website ? (
-              <Link href={location.website} passHref={true}>
-                <button>Homepage</button>
-              </Link>
-            ) : null}
+            <Link
+              href={`/travel/${location.item ? location.item.id : location.id}`}
+              passHref={true}
+            >
+              <h3>{location.item ? location.item.name : location.name}</h3>
+            </Link>
+            <p>{location.item ? location.item.caption : location.caption}</p>
+            <p>{location.item ? location.item.address : location.address}</p>
+            {pageLoaded ? (
+              <div>
+                {/*Website Check*/}
+                {location.item ? (
+                  location.item.website ? (
+                    <Link href={location.item.website} passHref={true}>
+                      <button>Homepage</button>
+                    </Link>
+                  ) : null
+                ) : location.website ? (
+                  <Link href={location.website} passHref={true}>
+                    <button>Homepage</button>
+                  </Link>
+                ) : null}
 
-            {/*Twitter Check*/}
-            {location.twitter ? (
-              <Link
-                href={`https://twitter.com/${location.twitter}`}
-                passHref={true}
-              >
-                <button>Twitter</button>
-              </Link>
-            ) : null}
+                {/*Twitter Check*/}
+                {location.item ? (
+                  location.item.twitter ? (
+                    <Link
+                      href={`https://twitter.com/${location.item.twitter}`}
+                      passHref={true}
+                    >
+                      <button>Twitter</button>
+                    </Link>
+                  ) : null
+                ) : location.twitter ? (
+                  <Link
+                    href={`https://twitter.com/${location.twitter}`}
+                    passHref={true}
+                  >
+                    <button>Twitter</button>
+                  </Link>
+                ) : null}
 
-            {/*Instagram Check*/}
-            {location.instagram ? (
-              <Link
-                href={`https://www.instagram.com/${location.instagram}/`}
-                passHref={true}
-              >
-                <button>Instagram</button>
-              </Link>
-            ) : null}
+                {/*Instagram Check*/}
+                {location.item ? (
+                  location.item.instagram ? (
+                    <Link
+                      href={`https://www.instagram.com/${location.item.instagram}/`}
+                      passHref={true}
+                    >
+                      <button>Instagram</button>
+                    </Link>
+                  ) : null
+                ) : location.instagram ? (
+                  <Link
+                    href={`https://www.instagram.com/${location.instagram}/`}
+                    passHref={true}
+                  >
+                    <button>Instagram</button>
+                  </Link>
+                ) : null}
+              </div>
+            ) : (
+              <div>Loading...</div>
+            )}
           </div>
-        ) : (
-          <div>Loading...</div>
-        )}
-      </div>
 
-      <div>
-        <iframe
-          src={location.map}
-          width="120"
-          height="120"
-          style={{ border: 0 }}
-          loading="lazy"
-        ></iframe>
-      </div>
-    </div>
-  ));
+          <div>
+            <iframe
+              src={location.item ? location.item.map : location.map}
+              width="120"
+              height="120"
+              style={{ border: 0 }}
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+      ))
+    );
+
+  console.log(locations);
 
   return (
     <>
@@ -114,7 +149,9 @@ const Travel = ({ data }) => {
             mobile ? styles.mobileTravelContainer : styles.travelPageContainer
           }
         >
-          {!mobile ? <SearchBar /> : null}
+          {!mobile ? (
+            <SearchBar data={data} setLocations={setLocations} />
+          ) : null}
           <div
             className={
               mobile
