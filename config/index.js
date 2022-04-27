d7eac7bc9f37d79b0e1e6f3112608315efd3a58d
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-27 19:25:17 +0900

    CONFIG INDEX WAS THE PROBLEM

--- config/index.js
+++
@@ -1,6 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-
-export const server = dev
-  ? "http://localhost:3000"
-  : // : "https://negineko-site.herokuapp.com";
-    "http://localhost:3000";
