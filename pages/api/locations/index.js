b32d2e3b8014ef31fc1dc24f497a1309ae6afc7f
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-28 13:39:27 +0900

    test

--- pages/api/locations/index.js
+++
@@ -1,8 +0,0 @@
-import prisma from "../../../lib/prisma";
-
-export default async function handler(req, res) {
-  const locations = await prisma.location.findMany();
-  if (req.method === "GET") {
-    res.status(200).json(locations);
-  }
-}
