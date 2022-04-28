0adec96482b766f3c750c0b181f441e09071198d
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-28 14:32:10 +0900

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,9 +3,13 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
-prisma = global.prisma;
 
 export default prisma;
