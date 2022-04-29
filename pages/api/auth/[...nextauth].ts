f5f7f25b14e6485c1c5f3a85103009c671387e54
Hung-Le <theorangedot@oranges-mini.lan>
2022-04-29 10:40:04 +0900

    adjusted env variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,4 +12,13 @@ export default NextAuth({
     // ...add more providers here
   ],
   session: { strategy: "jwt" },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
 });
