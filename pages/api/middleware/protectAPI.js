e637f2e687454920c84bf54f33f58e53c9af58eb
Hung-Le <theorangedot@oranges-mini.lan>
2022-05-09 11:30:42 +0900

    testing protectAPI

--- pages/api/middleware/protectAPI.js
+++ pages/api/middleware/protectAPI.js
@@ -2,10 +2,11 @@ import { server } from "../../../config";
 
 const protectAPI = (handler) => {
   return async (req, res) => {
-    if (!server.includes(req.rawHeaders[1])) {
+    if (`https://${req.headers.host}` !== server) {
       return res.status(403).json({ success: false, message: "Forbidden" });
     }
 
+    console.log(req.headers.host);
     return handler(req, res);
   };
 };
