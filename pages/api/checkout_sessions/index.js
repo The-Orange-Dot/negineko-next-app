0264caa8135d027fa74e22e13541cf8688f7d077
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-05-13 21:45:52 +0900

    Adding Stripe

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "price_1KyvOvCsaqCLx2xLMYIYfAl5",
+            price: req.body.product,
             quantity: 1,
           },
         ],
