318a73a5c9cbaeabc949203fc789d8ad0e3c1cd3
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-05-25 00:00:00 +0900

    Socketio error

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://negineko.netlify.app";
+  : "https://negineko.netlify.app:80";
