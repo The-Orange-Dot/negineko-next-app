48c5d2dc79f4d844a4f4798013c75fd3564136f1
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-05-25 17:19:23 +0900

    fixed socket fetch origin request

--- pages/api/socket/index.ts
+++ pages/api/socket/index.ts
@@ -21,7 +21,7 @@ const SocketHandler = async (
     // adapt Next's net Server to http Server
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
-      path: "/api/socket",
+      path: "/api/*",
       cors: {
         origin: "*:*",
         methods: ["GET", "POST"],
