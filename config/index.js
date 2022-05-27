250e54ee14f2a6620a0c422a90503f44f29381f4
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-05-27 16:21:15 +0900

    Added heroku support

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
+import { env } from "process";
+
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://negineko.netlify.app";
+export const server = dev ? "http://localhost:3000" : process.env.BASE_URL;
