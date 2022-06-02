f33af3e0e96207900ab45709ed6487d70d150198
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-06-02 16:24:52 +0900

    Adjusted more config files for amplify

--- config/index.js
+++ config/index.js
@@ -3,5 +3,3 @@ import { env } from "process";
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev ? "http://localhost:3000" : process.env.BASE_URL;
-
-const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "neginekotokyo.com";
