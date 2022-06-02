ce9d05610b0e7398ae2df7edcd035e8e0942a8eb
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-06-02 15:58:53 +0900

    config changes

--- config/index.js
+++ config/index.js
@@ -3,3 +3,5 @@ import { env } from "process";
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev ? "http://localhost:3000" : process.env.BASE_URL;
+
+const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "neginekotokyo.com";
